0f4d64c76fbf649cf196a65aa98fa8f332b5a395
Brian Kipkemoi Langat <briomyg@gmail.com>
2024-11-14 17:43:24 +0300

    Update update.sh
    Hunchobrian ob

--- update.sh
+++ update.sh
@@ -27,7 +27,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/ghoibvpn21/Autoscript/GM/menu/menu.zip
+    wget https://raw.githubusercontent.com/Hunchobrian/Autoscript/GM/menu/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
